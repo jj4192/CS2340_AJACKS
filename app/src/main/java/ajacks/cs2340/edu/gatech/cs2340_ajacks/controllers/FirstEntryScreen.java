32e565144387bc4f90ee1ca52f865e2ad4297771
Christine Hsieh <chsieh40@gatech.edu>
2017-09-22 11:27:39 -0400

    Add verification to login for hardcoded user and pass. Added First Entry Screen for when login is successful

---
+++ app/src/main/java/ajacks/cs2340/edu/gatech/cs2340_ajacks/controllers/FirstEntryScreen.java
@@ -0,0 +1,15 @@
+package ajacks.cs2340.edu.gatech.cs2340_ajacks.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ajacks.cs2340.edu.gatech.cs2340_ajacks.R;
+
+public class FirstEntryScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_entry_screen);
+    }
+}
--- app/src/main/java/ajacks/cs2340/edu/gatech/cs2340_ajacks/controllers/LoginScreen.java
+++ app/src/main/java/ajacks/cs2340/edu/gatech/cs2340_ajacks/controllers/LoginScreen.java
@@ -2,6 +2,10 @@ package ajacks.cs2340.edu.gatech.cs2340_ajacks.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
+
 
 import ajacks.cs2340.edu.gatech.cs2340_ajacks.R;
 
@@ -11,5 +15,37 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+    }
+
+    /**
+     * What happens when btn_login is clicked
+     * @param view
+     */
+    protected void onClick_btn_login(View view) {
+        //pull from text boxes
+        EditText username = (EditText) findViewById(R.id.tb_username);
+        EditText password = (EditText) findViewById(R.id.tb_password);
+        //check credentials
+        if (checkCredentials(username.getText().toString(), password.getText().toString()) == 1) {
+            Intent intent = new Intent(LoginScreen.this, FirstEntryScreen.class);
+            startActivity(intent);
+        } else {
+            //clear entry information
+            username.setText("");
+            password.setText("");
+        }
+    }
+
+    /**
+     * FOR NOW, returns 1 (success) if credentials are user and pass
+     * @param username
+     * @param password
+     * @return
+     */
+    private int checkCredentials(String username, String password) {
+        if (username.equals("user") && password.equals("pass")) {
+            return 1;
+        }
+        return 0;
     }
 }
