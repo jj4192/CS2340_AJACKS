3657ddecec214fd8097971ee84b18972f9384173
Sarah Li <gtsli469@gmail.com>
2017-09-24 14:22:07 -0400

    Added logout button UI in XML file and functionality in FirstEntryScreen.java

--- app/src/main/java/ajacks/cs2340/edu/gatech/cs2340_ajacks/controllers/FirstEntryScreen.java
+++ app/src/main/java/ajacks/cs2340/edu/gatech/cs2340_ajacks/controllers/FirstEntryScreen.java
@@ -1,7 +1,9 @@
 package ajacks.cs2340.edu.gatech.cs2340_ajacks.controllers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import ajacks.cs2340.edu.gatech.cs2340_ajacks.R;
 
@@ -11,5 +13,14 @@ public class FirstEntryScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_entry_screen);
+    }
+
+    /**
+     * What happens when btn_logout is clicked. Redirects to WelcomeScreen.
+     * @param view
+     */
+    protected void onClick_btn_logout(View view) {
+        Intent intent = new Intent(FirstEntryScreen.this, WelcomeScreen.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/ajacks/cs2340/gatech/edu/cs2340_ajacks/WelcomeScreen.java
+++ app/src/main/java/ajacks/cs2340/gatech/edu/cs2340_ajacks/WelcomeScreen.java
@@ -3,6 +3,8 @@ package ajacks.cs2340.gatech.edu.cs2340_ajacks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ajacks.cs2340.edu.gatech.cs2340_ajacks.R;
+
 public class WelcomeScreen extends AppCompatActivity {
 
     @Override
