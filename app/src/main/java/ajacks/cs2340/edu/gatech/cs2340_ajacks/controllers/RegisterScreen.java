dcedc16260ff0a63f6e893c361ffc918618b41a3
Alonzo Hernandez <alonzo.hernandez@gatech.edu>
2017-09-29 10:10:23 -0400

    Added a skeleton for the registration logic.

--- app/src/main/java/ajacks/cs2340/edu/gatech/cs2340_ajacks/controllers/RegisterScreen.java
+++ app/src/main/java/ajacks/cs2340/edu/gatech/cs2340_ajacks/controllers/RegisterScreen.java
@@ -1,10 +1,12 @@
 package ajacks.cs2340.edu.gatech.cs2340_ajacks.controllers;
 
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 import ajacks.cs2340.edu.gatech.cs2340_ajacks.R;
 
@@ -26,7 +28,20 @@ public class RegisterScreen extends AppCompatActivity {
     }
 
     protected void onClick_btn_register(View view) {
-        //register logic
+        boolean passChecked = false; //TODO(Kevin): add correct logic
+        EditText username = (EditText) findViewById(R.id.tb_username);
+        EditText password = (EditText) findViewById(R.id.tb_password);
+        if (passChecked) {
+            Intent intent = new Intent(RegisterScreen.this, FirstEntryScreen.class);
+            startActivity(intent);
+            //Do we want a successful signup make people login or should it log them in?
+        } else {
+            AlertDialog alertDialog = new AlertDialog.Builder(RegisterScreen.this).create();
+            alertDialog.setMessage("Signup failed.");
+            alertDialog.show();
+            username.setText("");
+            password.setText("");
+        }
     }
 }
 
