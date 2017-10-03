3d76659e41dc1b4a66b5789c9c2f2b457ca3032e
Kevin Chen <kevinchen3253@gmail.com>
2017-10-02 23:58:58 -0400

    Created singleton design

--- app/src/main/java/ajacks/cs2340/edu/gatech/cs2340_ajacks/model/Model.java
+++ app/src/main/java/ajacks/cs2340/edu/gatech/cs2340_ajacks/model/Model.java
@@ -1,5 +1,8 @@
 package ajacks.cs2340.edu.gatech.cs2340_ajacks.model;
 
+import android.os.Debug;
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,16 +12,35 @@ import java.util.List;
 
 public class Model {
 
+
+    private static final Model _instance = new Model();
+
+    /**
+     * Singleton design model. Create one instance of the model
+     * @return the singleton model
+     */
+    public static Model getInstance() {
+        return _instance;
+    }
+
     private List<User> allUsers;
 
     public Model() {
         allUsers = new ArrayList<User>();
     }
 
+    /**
+     * Creates a new user to add to the database
+     * @param newUser the new user object being added to the database
+     */
     public void addUser(User newUser) {
         allUsers.add(newUser);
     }
 
+    /**
+     * Getter method to return a list of all current users from the database
+     * @return list of all current users of type User
+     */
     public List<User> getAllUsers() {
         return allUsers;
     }
